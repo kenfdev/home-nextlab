919d523e87c54f44721f393bd4b92a065c20e0ff
Ken Fukuyama <kenfdev@gmail.com>
2018-07-15 22:59:53 +0900

    update

--- faas/slack-event-sub/handler.js
+++ faas/slack-event-sub/handler.js
@@ -1,6 +1,41 @@
-"use strict"
+'use strict';
+
+const { WebClient } = require('@slack/client');
 
 module.exports = (context, callback) => {
-    console.error(context);
-    callback(undefined, {headers: {"Content-Type": "text/plain"}, body: "success"});
-}
+  const oAuthToken = fs
+    .readFileSync(`/run/secrets/bot-user-oauth-access-token`)
+    .toString();
+  const verifyToken = fs
+    .readFileSync(`/run/secrets/slack-verify-token`)
+    .toString();
+
+  const {
+    data: { body }
+  } = JSON.parse(context);
+
+  if (body.token !== verifyToken) {
+    callback(undefined, {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'Token Invalid'
+    });
+    return;
+  }
+
+  callback(undefined, {
+    headers: { 'Content-Type': 'text/plain' },
+    body: 'success'
+  });
+
+  const web = new WebClient(oAuthToken);
+  web.chat
+    .postMessage({ channel: body.event.channel, text: 'Hello there' })
+    .then(res => {
+      // `res` contains information about the posted message
+      console.error('Message sent: ', res.ts);
+    })
+    .catch(console.error);
+};
