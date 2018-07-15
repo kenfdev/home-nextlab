b2b2b583442ed918ba5614e9cf99cd9eda856304
Ken Fukuyama <kenfdev@gmail.com>
2018-07-16 08:09:19 +0900

    update

--- faas/df-to-slack/handler.js
+++ faas/df-to-slack/handler.js
@@ -1,6 +1,63 @@
-"use strict"
+'use strict';
+
+const fs = require('fs');
+const { WebClient } = require('@slack/client');
+const request = require('request');
 
 module.exports = (context, callback) => {
-    console.error(context);
-    callback(undefined, {status: "done"});
-}
+  const oAuthToken = fs
+    .readFileSync(`/run/secrets/bot-user-oauth-access-token`)
+    .toString();
+
+  const { data } = JSON.parse(context);
+  const body = Buffer.from(data.body, 'base64');
+
+  const query = body.queryResult.queryText;
+  const web = new WebClient(oAuthToken);
+  web.chat
+    .postMessage({ channel: 'C994KQKG9', text: query })
+    .then(res => {
+      // `res` contains information about the posted message
+      console.error('Message sent: ', res.ts);
+
+      callback(undefined, {
+        body: {
+          payload: {
+            google: {
+              expectUserResponse: false,
+              richResponse: {
+                items: [
+                  {
+                    simpleResponse: {
+                      textToSpeech: '送りました'
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      callback(undefined, {
+        body: {
+          payload: {
+            google: {
+              expectUserResponse: false,
+              richResponse: {
+                items: [
+                  {
+                    simpleResponse: {
+                      textToSpeech: '送信に失敗しました'
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      });
+    });
+};
